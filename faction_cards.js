421ae4c581ae1e1ca797e40bd745996da00e55ee
Immelman51 <31988939+Immelman51@users.noreply.github.com>
2024-07-01 00:01:39 +0200

    Update faction_cards.js

--- faction_cards.js
+++ faction_cards.js
@@ -47,20 +47,24 @@ if(window.location.hash) {
     // Aucun hash n'est trouvé
 }
 function displayLeaders() { //on va d'abord faire afficher les leaders de la faction sélectionnée
-
+    
+    let pLeader = document.createElement('div'); //permet d'accéder à la section leaders via le menu du header
+    pLeader.setAttribute("id", "leaders");
+    
 for (let i = 0; i<leaders.length; i++){ 
     if (leaders[i]["leaderfaction"].includes(factionSelected)){
         let newleader = document.createElement('p');
         newleader.setAttribute("class", "leader");
+        
 
         let newleadername = document.createElement('div');
         newleadername.setAttribute("class", "leader name");
-        newleadername.textcontent = leaders[i]["leadername"];
+        newleadername.innerHTML = leaders[i]["leadername"];        
         newleader.appendChild(newleadername);
 
         let newleadercharge = document.createElement('div');
         newleadercharge.setAttribute("class", "leader charge");
-        newleadercharge.textContent = leaders[i]["charge"];
+        newleadercharge.innerHTML = leaders[i]["charge"];
         newleader.appendChild(newleadercharge);
 
         for (let j=0; j<3 ; j++){
@@ -80,6 +84,53 @@ for (let i = 0; i<leaders.length; i++){
     }
 }
 };
+function displayShipsandPilots() { //on va d'abord faire afficher les ships et les pilots associés
+
+    let pPilots = document.createElement('div'); //permet d'accéder à la section pilots via le menu du header
+    pPilots.setAttribute("id", "pilots");
+
+for (let i=0 ; i<ships.length; i++){
+    if (ships[i]["factions"].includes(factionSelected)){
+        let newship = document.createElement('p');
+        newship.setAttribute("class","ship");
+
+        let newshipname = document.createElement('div');
+        newshipname.setAttribute("class", "ship name");
+        newshipname.innerHTML = ships[i]["name"];        
+        newship.appendChild(newshipname);
+
+        let newshipattack = document.createElement('div');
+        newshipattack.setAttribute("class", "ship attack");
+        let attributesNames = ships[i].getAttributeNames(); //on va voir tester les différents les noms des différents noms d'attributs pour détecter ceux qui visent les attaques
+        for (let j = 0 ; attributesNames.length ; j++){
+            switch(attributesNames[j]){
+                case 'attack': 
+                let attack1 = createElement('div');
+                attack1.setAttribute("src",'img/attack.jpg');
+                attack1.innerHTML = ships[i]["attack"];
+                break;
+                case 'attackt' :
+            }
+        }
+        newship.appendChild(newshipattack);
+
+        for (let j=0; j<3 ; j++){
+            let factions = leaders[i]["leaderfaction"][j];
+            let factionlogo = document.createElement("div");
+            factionlogo.setAttribute("class", "leader faction");
+            factionlogo.setAttribute("src",'img/'+factions+'mini.jpg');
+            newleader.appendChild(factionlogo);
+        }
+        let newleaderability = document.createElement("div");
+        newleaderability.textContent = leaders[i]["leaderability"];
+        newleaderability.setAttribute("class", "leader ability");
+        newleader.appendChild(newleaderability);
+
+        document.body.appendChild(newleader);
+    }
+}
+
+}
 
 // Load all data and then execute the rest
 loadAllData();
