1a54f662cd5ad1c5c0f4b69013f80a710a0b9bb2
Immelman51 <31988939+Immelman51@users.noreply.github.com>
2024-09-30 22:32:31 +0200

    création de display leader() mais il y a un problème car leaders n'a pas le tremms de se charger pour que ça puisse apparaitre

--- print_squad.js
+++ print_squad.js
@@ -2,8 +2,9 @@ let requestURLships = "https://raw.githubusercontent.com/Immelman51/xwunited/mai
 let requestURLchassis = "https://raw.githubusercontent.com/Immelman51/xwunited/main/chassis.json";
 let requestURLpilots = "https://raw.githubusercontent.com/Immelman51/xwunited/main/pilots.json";
 let requestURLupgrades = "https://raw.githubusercontent.com/Immelman51/xwunited/main/upgrades.json";
+let requestURLleaders = "https://raw.githubusercontent.com/Immelman51/xwunited/main/leaders.json";
 
-let ships, chassis, pilots, upgrades;
+let ships, chassis, pilots, upgrades, leaders;
 
 async function fetchData(url) {
     let response = await fetch(url);
@@ -20,30 +21,56 @@ async function fetchData(url) {
         chassis = await fetchData(requestURLchassis);
         pilots = await fetchData(requestURLpilots);
         upgrades = await fetchData(requestURLupgrades);
+        leaders = await fetchData(requestURLleaders);
         
         console.log(ships);
         console.log(chassis);
         console.log(pilots);
         console.log(upgrades);
+        console.log(leaders);
 
         // Vous pouvez maintenant utiliser les variables ships, chassis, pilots, et upgrades ici ou dans d'autres fonctions
     } catch (error) {
         console.error("Failed to fetch data: ", error);
     }
 })();
-
+ let indexes = [];
 // Function to get the indexes from the URL hash
 function getIndexesFromHash() {
     // Get the hash from the URL
     const hash = window.location.hash.substring(1);
 
     // Split the hash into an array
-    const indexes = hash.split(',');
+    indexes = hash.split(',');
 
     return indexes;
 }
 // The array 'indexes' contains ! ["leaderID","pilotID+u+upgrade1ID+u+upgrade2ID","pilotID"....]
 
-window.onload = getIndexesFromHash();
+
+function displayLeader(){
+const lID = indexes[0];
+const leaderName = document.getElementById('lname');
+const faction1 = document.getElementById('faction1');
+const faction2 = document.getElementById('faction2');
+const faction3 = document.getElementById('faction3');
+const leaderAbility = document.getElementById('lability');
+const leaderCharge = document.getElementById('lcharge');
+leaderName.textContent = leaders[lID]['leadername'];
+faction1.setAttribute("src",'img/'+leaders[lID]['leaderfaction'][0]+'mini.jpg') ;
+faction2.setAttribute("src",'img/'+leaders[lID]['leaderfaction'][1]+'mini.jpg') ;
+faction3.setAttribute("src",'img/'+leaders[lID]['leaderfaction'][2]+'mini.jpg') ;
+leaderAbility.textContent = leaders[lID]['leaderability'];
+leaderCharge.textContent = leaders[lID]['leadercharge'];
+
+
+}
+
+
+window.onload = function(){
+    getIndexesFromHash();
+    displayLeader();
+}
+
 
 
