a48f47255147f78e5396720ed8422f576865e5c0
Immelman51 <31988939+Immelman51@users.noreply.github.com>
2025-06-23 14:25:43 +0200

    finalisation des cartes scenario environment et wildcards. ainsi que le script de sceario.js

--- scenario.js
+++ scenario.js
@@ -25,7 +25,30 @@ for(i=1; i<4 ; i++){
     document.getElementById('assault'+i).setAttribute('src','img/scenario/TheAssault'+i+'_'+language+'.png');
 }
 
+for(i=1; i<3 ; i++){
+    document.getElementById('binarysuns'+i).setAttribute('src','img/scenario/Env_BinarySuns'+i+'_'+language+'.png');
 }
+for(i=1; i<3 ; i++){
+    document.getElementById('intergalacticvoid'+i).setAttribute('src','img/scenario/Env_Intergalacticvoid'+i+'_'+language+'.png');
+}
+for(i=1; i<3 ; i++){
+    document.getElementById('junkyard'+i).setAttribute('src','img/scenario/Env_Junkyard'+i+'_'+language+'.png');
+}
+for(i=1; i<3 ; i++){
+    document.getElementById('oldwreckage'+i).setAttribute('src','img/scenario/Env_OldWreckage'+i+'_'+language+'.png');
+}
+for(i=1; i<3 ; i++){
+    document.getElementById('planetaryring'+i).setAttribute('src','img/scenario/Env_PlanetaryRing'+i+'_'+language+'.png');
+}
+
+document.getElementById('cosmicanomaly').setAttribute('src','img/scenario/Wild_CosmicAnomaly_'+language+'.png');
+document.getElementById('fracturedsensors').setAttribute('src','img/scenario/Wild_FracturedSensors_'+language+'.png');
+document.getElementById('outpost').setAttribute('src','img/scenario/Wild_Outpost_'+language+'.png');
+document.getElementById('solarwinds').setAttribute('src','img/scenario/Wild_SolarWinds_'+language+'.png');
+document.getElementById('theyexplodenow').setAttribute('src','img/scenario/Wild_TheyExplodeNow_'+language+'.png');
+
+}
+
 
 //we listen to the language menu. If it changes, we change the language by switching the web adresses of the picture cards.
 document.getElementById('language').addEventListener('input',update_language);
