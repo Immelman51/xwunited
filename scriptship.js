739b1744436f27a3f47c08ad76425b5cd6eedf6d
Immelman51 <31988939+Immelman51@users.noreply.github.com>
2026-04-09 00:12:16 +0200

    Update scriptship.js

--- scriptship.js
+++ scriptship.js
@@ -458,10 +458,7 @@ function testRestriction (yy,tableRestrictions){//va vérifier si les restrictio
     }
    
     for (i=0; i<varlist.length; i++) {
-        console.log(JSON.stringify(varlist[i]));
-        console.log(JSON.stringify(target1));
-        console.log(varlist);
-        console.log(target1);
+        console.log(tableRestrictions);
         
         
     if ((JSON.stringify(varlist[i])===JSON.stringify(target1))||(JSON.stringify(varlist[i])===JSON.stringify(target2))) { //Json.stringify allows to compare to arrays together such as the actions arrays. It works as well on strings and values.
